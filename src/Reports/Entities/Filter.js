72099a13f892bc3dacc7896ecd90de6479845caf
artificial-life <proteus.protocol@gmail.com>
2016-04-13 14:56:35 +0600

    upd

--- src/Reports/Entities/Filter.js
+++ src/Reports/Entities/Filter.js
@@ -4,89 +4,87 @@
 // 	return (result) => _.reduce(fns, (a, f) => f.call(this, a), result);
 // };
 
-const operations = ['=', '<', '>', '<=', '>=', '!=', 'in'];
+const operations = ['===', '<=', '>=', '!=', '=', '<', '>', 'in'];
 
-let composer = function(fns) {
-  this.fns = fns;
+let composer = function (fns) {
+	this.fns = fns;
 };
 
-composer.prototype.filter = function(d) {
-  return _.reduce(this.fns, (a, f) => a = a && f(d), true)
+composer.prototype.filter = function (d) {
+	return _.reduce(this.fns, (a, f) => a = a && f(d), true)
 };
 
 let Filter = {
-  compose(type, names) {
-    let filters_functions = _.map(names, desc => this.isCondition(desc) ? this.parse(desc) : this.discover(entity, desc));
+	compose(type, names) {
+		let filters_functions = _.map(names, desc => this.isCondition(desc) ? this.parse(desc) : this.discover(entity, desc));
 
-    return new composer(filters_functions);
-  },
-  discover(type, name) {
+		return (d) => _.reduce(filters_functions, (a, f) => a = a && f(d), true);
+	},
+	discover(type, name) {},
+	isCondition(desc) {
+		let result = false;
+		_.forEach(operations, op => {
+			if (!~desc.indexOf(op)) return true;
+			result = true;
+			return false;
+		});
 
-  },
+		return result;
+	},
+	parse(condition) {
+		let operation = 'nope';
+		let fn;
+		_.forEach(operations, op => {
+			if (~condition.indexOf(op)) {
+				operation = op;
+				return false;
+			}
+		});
 
-  isCondition(desc) {
-    let result = false;
-    _.forEach(operations, op => {
-      if (~desc.indexOf(op)) {
-        result = true;
-        return false;
-      }
-    });
+		let {
+			field,
+			value
+		} = this.extractFieldAndValue(operation, condition);
 
-    return result;
-  },
-  parse(condition) {
-    //@WARNING: it use reversed order of operands
-    let operation = 'nope';
-    let fn;
+		console.log(operation, field, value);
 
-    _.forEach(operations, op => {
-      if (~condition.indexOf(op)) {
-        operation = op;
-        return false;
-      }
-    });
-
-    let {
-      field,
-      value
-    } = this.extractFieldAndValue(operation, condition);
-
-    switch (operation) {
-      case '=':
-        fn = (x) => x[field] == value;
-        break;
-      case '>':
-        fn = (x) => x[field] > value;
-        break;
-      case '<':
-        fn = (x) => x[field] < value;
-        break;
-      case '<=':
-        fn = (x) => x[field] <= value;
-        break;
-      case '>=':
-        fn = (x) => x[field] >= value;
-        break;
-      case '!=':
-        fn = (x) => x[field] != value;
-        break;
-      case 'in':
-        fn = (x) => !!~value.indexOf(x[field]);
-        break;
-      default:
-        throw new Error(`Unknown operation ${operation}`);
-    }
-
-    return fn;
-  },
-  extractFieldAndValue(operation, condition) {
-    let parts = condition.split('operation')[0];
-    return {
-      field: _.trim(parts[0]),
-      value: _.trim(parts[1])
-    };
-  }
+		switch (operation) {
+		case '=':
+			fn = (x) => x[field] == value;
+			break;
+		case '===':
+			fn = (x) => x[field] === value;
+			break;
+		case '>':
+			fn = (x) => x[field] > value;
+			break;
+		case '<':
+			fn = (x) => x[field] < value;
+			break;
+		case '<=':
+			fn = (x) => x[field] <= value;
+			break;
+		case '>=':
+			fn = (x) => x[field] >= value;
+			break;
+		case '!=':
+			fn = (x) => x[field] != value;
+			break;
+		case 'in':
+			fn = (x) => !!~value.indexOf(x[field]);
+			break;
+		default:
+			throw new Error(`Unknown operation ${operation}`);
+		}
+		return fn;
+	},
+	extractFieldAndValue(operation, condition) {
+		let parts = condition.split(operation);
+		return {
+			field: _.trim(parts[0]),
+			value: _.trim(parts[1])
+		};
+	}
 };
 
 module.exports = Filter;
--- src/Reports/reports.js
+++ src/Reports/reports.js
@@ -8,18 +8,19 @@ require('moment-range');
 
 //@NOTE: it's draft
 let table_draft = {
-  interval: [0, 1000],
-  entity: "Ticket",
-  interval_field: "dedicated_date",
-  params: [{
-    label: "Param 1",
-    group: ['month', 'week-day', '30min'],
-    aggregator: "sum", //String const
-    filter: ["state = registered"]
-  }, {
-    label: "Param 2"
-      //...
-  }]
+	interval: [0, 1000],
+	entity: "Ticket",
+	interval_field: "dedicated_date",
+	params: [{
+		label: "Param 1",
+		field: "key",
+		group: ['month', 'week-day', '30min'],
+		aggregator: "sum", //String const
+		filter: ["state != registered"]
+	}, {
+		label: "Param 2"
+			//...
+	}]
 };
 
 
@@ -28,81 +29,90 @@ let Filters = require('./Entities/Filter.js');
 let Aggregators = require('./Entities/Aggregator.js');
 
 class Reports {
-  constructor() {
-    this.emitter = emitter;
-  }
-  init(config) {
-    this.tickets = new TicketApi();
-    this.tickets.initContent();
-  }
-  launch() {
-    let entity_name = table_draft.entity;
-    let group_names = table_draft.params[0].group;
-    let filters = table_draft.params[0].filter;
-    let group = Splitters.compose(entity_name, group_names);
-    let composed = Filters.compose(entity_name, filters);
-    setTimeout(() => {
+	constructor() {
+		this.emitter = emitter;
+	}
+	init(config) {
+		this.tickets = new TicketApi();
+		this.tickets.initContent();
+	}
+	launch() {
 
-      for (let i = 0; i < 10; i++) {
-        let d = moment();
-        d.add(-1 * i, 'days');
-        this.getTickets({
-          query: {
-            dedicated_date: d,
-            org_destination: 'department-1'
-          }
-        }).then((r) => {
-          _.forEach(r, t => {
-            console.log(group(t.booking_date));
-            console.log(composed.filter(t));
-            console.log(t.state);
-          });
+		let entity_name = table_draft.entity;
+		let group_names = table_draft.params[0].group;
+		let filters = table_draft.params[0].filter;
+		let group = Splitters.compose(entity_name, group_names);
+		let filter = Filters.compose(entity_name, filters);
 
-        });
+		let last;
+		setTimeout(() => {
+			let d = moment("2016-04-11");
+			this.getTickets({
+				query: {
+					dedicated_date: d,
+					org_destination: 'department-1'
+				}
+			}).then((r) => {
+				console.log('response');
+				console.log(r.length);
+				var time = process.hrtime();
+				_.forEach(r, t => {
+					// console.log(t.booking_date);
+					// console.log(group(t.booking_date));
+					// console.log(composed.filter(t));
+					if (filter(t)) {
+						console.log(t[table_draft.params[0].field]);
+					}
+					// console.log(t.state);
+				});
 
-      }
-    }, 5000);
+
+				var diff = process.hrtime(time);
+
+				console.log('benchmark took %d msec', (diff[0] * 1e9 + diff[1]) / 1000000);
+			});
+		}, 5000);
 
 
 
-    return Promise.resolve(true);
-  }
-  actionGetTable(table) {
+		return Promise.resolve(true);
+	}
+	actionGetTable(table) {
 
-  }
-  getKeybuilder(entity) {
+	}
+	getKeybuilder(entity) {
 
-  }
-  parseParams(param) {
+	}
+	parseParams(param) {
 
-  }
-  composeFilter(entity, filters) {
-    let filters_functions = _.map(filters, desc => _.isObject(desc) ? this.findFilter(entity, desc) : this.parseFilter(desc));
+	}
+	composeFilter(entity, filters) {
+		let filters_functions = _.map(filters, desc => _.isObject(desc) ? this.findFilter(entity, desc) : this.parseFilter(desc));
 
-    return sequence(filters_functions);
-  }
-  findFilter(entity, name) {
+		return sequence(filters_functions);
+	}
+	findFilter(entity, name) {
 
-  }
-  parseFilter(condition) {
+	}
+	parseFilter(condition) {
 
-  }
+	}
 
-  //API
-  getTickets({
-    query,
-    keys
-  }) {
-    return this.emitter.addTask('ticket', {
-        _action: 'ticket',
-        query,
-        keys
-      })
-      .then((res) => {
-        // console.log("RES Q", res, query);
-        return _.values(res);
-      });
-  }
+	//API
+	getTickets({
+		query,
+		keys
+	}) {
+		return this.emitter.addTask('ticket', {
+				_action: 'ticket',
+				query,
+				keys
+			})
+			.then((res) => {
+				// console.log("RES Q", res, query);
+				return _.values(res);
+			});
+	}
 
 }
 module.exports = Reports;
