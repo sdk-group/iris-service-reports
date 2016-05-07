5c6b1ae487f5f7c0641234269009eceedc82fbd6
artificial-life <proteus.protocol@gmail.com>
2016-05-07 19:56:47 +0600

    tests

--- src/Reports/Entities/DataSource.js
+++ src/Reports/Entities/DataSource.js
@@ -1,11 +1,33 @@
 'use strict'
 
+/*@NOTE: test*/
+let RDFcb = require("cbird-rdf").RD;
+let cfg = {
+	"couchbird": {
+		"server_ip": "194.226.171.100",
+		"n1ql": "194.226.171.100:8093"
+	},
+	"buckets": {
+		"main": "rdf",
+		"auth": "ss",
+		"history": "rdf"
+	},
+	"filename": "./output.json"
+};
+let db = new RDFcb(cfg.couchbird);
+let main_bucket = db.bucket(cfg.buckets.main);
+/*@NOTE: test*/
+
+
 let message_bus = require('global-queue');
 
+let makeKey = (org, dedicated_date) => {
+	let dd = _.isString(dedicated_date) ? dedicated_date : dedicated_date.format("YYYY-MM-DD");
+	return `ticket-${org}-${dd}`;
+};
+
 class Source {
-	constructor() {
-		this.concurrency = 3;
-	}
+	constructor() {}
 	setInterval(value) {
 		this.interval = value;
 		return this;
@@ -14,37 +36,81 @@ class Source {
 		this.departments = _.castArray(value);
 		return this;
 	}
-	parse(callback) {
 
+	// parse(callback) {
+	//
+	// 	let response_counter = 0;
+	// 	let days = this.getDays();
+	// 	let set = [];
+	// 	let first_date = _.head(days);
+	// 	let last_date = _.last(days);
+	//
+	// 	_.forEach(days, d => _.forEach(this.departments, dept => set.push({
+	// 		day: d,
+	// 		department: dept
+	// 	})));
+	// 	//@TODO: use here iterator, not array
+	// 	// console.log(set);
+	//
+	// 	Promise.map(set, item => this.getTickets({
+	// 			query: {
+	// 				dedicated_date: item.day,
+	// 				org_destination: item.department
+	// 			}
+	// 		})
+	// 		.then((data) => {
+	// 			if (item.day == first_date) return this._preFilter(data, first_date);
+	// 			if (item.day == last_date) return this._postFilter(data, last_date);
+	// 			return data;
+	// 		})
+	// 		.then(callback)
+	// 		.then(() => {
+	// 			if ((response_counter += 1) == (days.length * this.departments.length)) this.final();
+	//
+	// 			return true;
+	// 		}));
+	//
+	// 	return this;
+	// }
+
+	parse(callback) {
 		let response_counter = 0;
 		let days = this.getDays();
-		let set = [];
 		let first_date = _.head(days);
 		let last_date = _.last(days);
 
-		_.forEach(days, d => _.forEach(this.departments, dept => set.push({
-			day: d,
-			department: dept
-		})));
-		//@TODO: use here iterator, not array
-		Promise.map(set, item => this.getTickets({
-				query: {
-					dedicated_date: item.day,
-					org_destination: item.department
+		let keys = [];
+		let delimiter = '--';
+
+		_.forEach(days, d => _.forEach(this.departments, dept => {
+			keys.push(makeKey(dept, d));
+		}));
+		var chunktime = process.hrtime();
+		main_bucket.getMulti(_.map(keys, k => `counter-${k}`)).then(d => {
+				return d;
+			}).then(data => _.reduce(data, (a, d, n) => {
+				if (!!d.value) {
+					let key = n.slice(8);
+					a.push(_.map(_.range(d.value), (num) => `${key}${delimiter}${num}`))
 				}
+				return a;
+			}, []))
+			.then(d => _.flatten(d))
+			.then(d => {
+				console.log(d.length);
+				return d;
 			})
-			.then((data) => {
-				if (item.day == first_date) return this._preFilter(data, first_date);
-				if (item.day == last_date) return this._postFilter(data, last_date);
-				return data;
+			.then(d => main_bucket.getMulti(d))
+			.then(d => {
+				console.log(_.size(d));
+				var chdiff = process.hrtime(chunktime);
+				console.log('chunk took %d msec', (chdiff[0] * 1e9 + chdiff[1]) / 1000000);
+				return d;
 			})
 			.then(callback)
 			.then(() => {
-				if ((response_counter += 1) == (days.length * this.departments.length)) this.final();
-
+				this.final();
 				return true;
-			}), {
-				concurrency: this.concurrency
 			});
 
 		return this;
--- src/Reports/reports.js
+++ src/Reports/reports.js
@@ -13,74 +13,76 @@ let Aggregators = require('./Entities/Aggregator.js');
 let DataSource = require('./Entities/DataSource.js');
 
 class Reports {
-  constructor() {
-    this.emitter = emitter;
-  }
-  init(config) {}
-  launch() {
-    return Promise.resolve(true);
-  }
+	constructor() {
+		this.emitter = emitter;
+	}
+	init(config) {}
+	launch() {
+		return Promise.resolve(true);
+	}
 
-  //API
-  actionGetTable({
-    table
-  }) {
-    var time = process.hrtime();
-    let rows = table.params;
-    let entity_name = table.entity;
+	//API
+	actionGetTable({
+		table
+	}) {
+		var time = process.hrtime();
+		let rows = table.params;
+		let entity_name = table.entity;
 
-    let source = DataSource.discover(entity_name, table.interval_field);
+		let source = DataSource.discover(entity_name, table.interval_field);
 
-    source.setInterval(table.interval)
-      .setDepartments(table.department);
-    let group = Splitters.compose(entity_name, table.group);
+		source.setInterval(table.interval)
+			.setDepartments(table.department);
+		let group = Splitters.compose(entity_name, table.group);
 
-    let fns = _.mapValues(rows, row => ({
-      filter: Filters.compose(entity_name, row.filter),
-      aggregator: Aggregators.get(entity_name, row.aggregator)
-    }));
+		let fns = _.mapValues(rows, row => ({
+			filter: Filters.compose(entity_name, row.filter),
+			aggregator: Aggregators.get(entity_name, row.aggregator)
+		}));
 
-    let accumulator = {};
-    let meta = {};
+		let accumulator = {};
+		let meta = {};
 
-    let result = new Promise(function(resolve, reject) {
-      source.parse((data) => {
-        _.forEach(rows, (row, index) => {
-          let key = row.key;
-          let meta_key = row.meta;
-          let filter = _.get(fns, [index, 'filter']);
+		let result = new Promise(function (resolve, reject) {
+			source.parse((data) => {
+				var chunktime = process.hrtime();
+				_.forEach(rows, (row, index) => {
+					let key = row.key;
+					let meta_key = row.meta;
+					let filter = _.get(fns, [index, 'filter']);
 
-          _.forEach(data, (data_row) => {
-            if (!filter(data_row)) return true;
-            let group_index = group(data_row);
-            let exported = key ? data_row[key] : 1;
-            _.updateWith(accumulator, [group_index, index], (n) => n ? (n.push(exported) && n) : [exported], Object);
-            if (meta_key) _.updateWith(meta, [group_index, index], (n) => n ? (n.push(data_row[meta_key]) && n) : [data_row[meta_key]], Object);
-          })
+					_.forEach(data, (data_row) => {
+						if (!filter(data_row)) return true;
+						let group_index = group(data_row);
+						let exported = key ? data_row[key] : 1;
+						_.updateWith(accumulator, [group_index, index], (n) => n ? (n.push(exported) && n) : [exported], Object);
+						if (meta_key) _.updateWith(meta, [group_index, index], (n) => n ? (n.push(data_row[meta_key]) && n) : [data_row[meta_key]], Object);
+					})
+				})
+				var chdiff = process.hrtime(chunktime);
+				console.log('chunk took %d msec', (chdiff[0] * 1e9 + chdiff[1]) / 1000000);
+			}).finally(() => {
+				let result = _.mapValues(accumulator, (group, group_index) => _.mapValues(group, (d, param_index) => {
+					let value = fns[param_index].aggregator(d);
+					return table.params[param_index].meta ? {
+						value: value,
+						meta: _.get(meta, [group, index])
+					} : value;
+				}));
 
-        })
-      }).finally(() => {
-        let result = _.mapValues(accumulator, (group, group_index) => _.mapValues(group, (d, param_index) => {
-          let value = fns[param_index].aggregator(d);
-          return table.params[param_index].meta ? {
-            value: value,
-            meta: _.get(meta, [group, index])
-          } : value;
-        }));
+				var diff = process.hrtime(time);
+				console.log('table took %d msec', (diff[0] * 1e9 + diff[1]) / 1000000);
 
-        var diff = process.hrtime(time);
-        console.log('table took %d msec', (diff[0] * 1e9 + diff[1]) / 1000000);
+				resolve(result);
+			});
+		});
 
-        resolve(result);
-      });
-    });
-
-    return result;
-  }
-  actionGetTableTemplate() {
-    //@NOTE: get template from DB
-    //return Template_Object
-  }
+		return result;
+	}
+	actionGetTableTemplate() {
+		//@NOTE: get template from DB
+		//return Template_Object
+	}
 
 
 }
