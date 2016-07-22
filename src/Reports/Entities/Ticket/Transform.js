d3c672c63b9dd0300c1fb7db6e70af2144aab6d9
artificial-life <proteus.protocol@gmail.com>
2016-07-22 17:29:25 +0600

    upd

--- src/Reports/Entities/Ticket/Transform.js
+++ src/Reports/Entities/Ticket/Transform.js
@@ -5,14 +5,21 @@ let _ = require('lodash');
 let TicketTransforms = {
 	waitingTime(ticket) {
 		var register = _.find(ticket.history, ['event_name', 'register']) || _.find(ticket.history, ['event_name', 'activate']);
-		var call = _.find(ticket.history, ['event_name', 'call']) || {
-			time: Date.now()
-		};
+		var call = _.find(ticket.history, ['event_name', 'call']);
+
+		if (!call && register) {
+			let day_end = (new Date(register.time)).setHours(23, 59, 59);
+			let now = Date.now();
+			call = now < day_end ? {
+				time: now
+			} : false;
+		}
 
 		if (!register || !call) {
 			ticket.waitingTime = -1;
 			return;
 		}
+
 		ticket.waitingTime = call.time - register.time;
 	}
 };
