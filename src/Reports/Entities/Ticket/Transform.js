f6ca2007cbe9d5719d7ed0e9d276db4fb4ba7c2d
artificial-life <proteus.protocol@gmail.com>
2016-09-07 15:34:34 +0600

    fix oO

--- src/Reports/Entities/Ticket/Transform.js
+++ src/Reports/Entities/Ticket/Transform.js
@@ -5,6 +5,7 @@ let _ = require('lodash');
 let TicketTransforms = {
 	waitingTime(ticket) {
 
+		var register = _.findLast(ticket.history, ['event_name', 'route']) || _.find(ticket.history, ['event_name', 'register']) || _.find(ticket.history, ['event_name', 'activate']);
 		var call = _.find(ticket.history, ['event_name', 'call']);
 
 		if (!call && register) {
