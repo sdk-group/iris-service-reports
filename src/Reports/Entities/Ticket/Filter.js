136ed5d17ac35deb428f88e1ca9e93de4452c3e7
artificial-life <proteus.protocol@gmail.com>
2016-09-22 07:47:54 +0700

    upd

--- src/Reports/Entities/Ticket/Filter.js
+++ src/Reports/Entities/Ticket/Filter.js
@@ -1,15 +1,15 @@
 'use strict'
 
-const event_names = ['register', 'call', 'activate', 'close', 'processing', 'book', 'postpone', 'expire'];
+const event_names = ['register', 'call', 'activate', 'close', 'processing', 'book', 'postpone', 'expire', 'qa-check'];
 
 let inHistory = (ticket, event) => ~_.findIndex(ticket.history, ['event_name', event]);
 
 let TicketFilter = {};
 
 
-_.forEach(event_names, event => TicketFilter['hasEvent' + _.upperFirst(event)] = (ticket) => inHistory(ticket, event));
+_.forEach(event_names, event => TicketFilter[_.camelCase('hasEvent-' + event)] = (ticket) => inHistory(ticket, event));
 //@NOTE: "not" functions
-_.forEach(event_names, event => TicketFilter['!hasEvent' + _.upperFirst(event)] = (ticket) => !(inHistory(ticket, event)));
+_.forEach(event_names, event => TicketFilter['!' + _.camelCase('hasEvent-' + event)] = (ticket) => !(inHistory(ticket, event)));
 
 
 module.exports = TicketFilter;
